81e90b9296c4c941974f1e4f71aea3c805a99c2a
Edwin Thomas <edthom444@gmail.com>
2024-03-20 18:21:57 -0400

    New approach based on google play location services

--- app/src/main/java/com/tutorial/activityrecognition/Constants.kt
+++ app/src/main/java/com/tutorial/activityrecognition/Constants.kt
@@ -1,6 +1,5 @@
 package com.tutorial.activityrecognition
 
 object Constants {
-    const val TRANSITIONS_RECEIVER_ACTION = "TRANSITIONS_RECEIVER_ACTION"
-    const val MY_PERMISSIONS_REQUEST_ACTIVITY_RECOGNITION = 50
+    const val REQUEST_CODE = 100
 }
--- app/src/main/java/com/tutorial/activityrecognition/MainActivity.kt
+++ app/src/main/java/com/tutorial/activityrecognition/MainActivity.kt
@@ -1,96 +1,150 @@
 package com.tutorial.activityrecognition
 
-import android.annotation.SuppressLint
-import android.app.AlarmManager
-import android.app.PendingIntent
-import android.app.PendingIntent.FLAG_UPDATE_CURRENT
+import android.Manifest
 import android.content.ContentValues
-import android.content.Context
-import android.content.Intent
-import android.content.IntentFilter
 import android.content.pm.PackageManager
-import android.os.Build
 import android.os.Bundle
-import android.os.SystemClock
 import android.util.Log
-import androidx.annotation.RequiresApi
-import androidx.appcompat.app.AppCompatActivity
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
 import androidx.core.app.ActivityCompat
-import androidx.core.app.PendingIntentCompat.send
-import androidx.core.content.ContextCompat
-import com.google.android.gms.location.ActivityRecognition
-import java.util.Calendar
-import java.util.Locale.Category
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
+import com.tutorial.activityrecognition.ui.theme.ActivityRecognitionTheme
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 
-lateinit var activityContext: Context
-class MainActivity : AppCompatActivity() {
-//    private val transitionReciever = ActivityTransitionBroadcastReceiver()
-//    private val TRANSITIONS_RECEIVER_ACTION = BuildConfig.APPLICATION_ID + "TRANSITIONS_RECEIVER_ACTION"
-//    private val MY_PERMISSIONS_REQUEST_ACTIVITY_RECOGNITION = 50
-    private val reciever = ActivityTransitionBroadcastReceiver()
+class MainActivity : ComponentActivity() {
 
-    @RequiresApi(Build.VERSION_CODES.Q)
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var locationRequest: LocationRequest
+    private lateinit var locationCallback: LocationCallback
+
+    private var locationMem = mutableListOf<Pair<Double, Double>>()
+    private var position = -1
+    private var positionMem = -1
+    private var observationWin = mutableListOf<Int>()
+    val maxObservations = 20
+    var recognizedActivity by mutableStateOf(0)
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-//        reciever.setActivityContext(this)
-        // registering receiver
-        registerReceiver(
-            reciever,
-            IntentFilter(Constants.TRANSITIONS_RECEIVER_ACTION)
-        )
+        fusedLocationClient = FusedLocationProviderClient(this)
+        locationRequest = LocationRequest.create().apply {
+            interval = 2000 // 2 seconds
+            fastestInterval = 1000 // 1 second
+            priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        }
 
-//        val intent = Intent(this, ActivityTransitionBroadcastReceiver::class.java)
-        val intent = Intent(Constants.TRANSITIONS_RECEIVER_ACTION)
-        activityContext = this
-//        val intent = Intent("TRANSITION_ACTION")
+        locationCallback = object : LocationCallback() {
+            override fun onLocationResult(locationResult: LocationResult?) {
+                locationResult ?: return
+                // val currTime = SystemClock.elapsedRealtimeNanos()
+                // val thresholdTime = 2 * 1e9// half a minute for the threshold
+                for (location in locationResult.locations) {
+                    //val timeDiff = currTime - location.elapsedRealtimeNanos
+                    // Log.e(TAG, "Time Diff: ${timeDiff} ${location.speed}")
+                    position = (position+1)%2
+                    positionMem = (positionMem+1)%maxObservations
 
-        val pendingIntent = PendingIntent.getBroadcast(this, 0, intent, FLAG_UPDATE_CURRENT)
-        val transitionRequest = getTransitionRequests()
-//        pendingIntent.send()
+                    var status = 0
+                    if (locationMem.size ==2){
+                        locationMem[position] = Pair(location.latitude, location.longitude)
 
+                        var diff_lat = Math.abs(locationMem[0].first - locationMem[1].first)
+                        var diff_long = Math.abs(locationMem[0].second - locationMem[1].second)
 
-        // check for permissions
-        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACTIVITY_RECOGNITION)
-            != PackageManager.PERMISSION_GRANTED) {
-            // ask for permission
-            ActivityCompat.requestPermissions(this,
-                arrayOf(android.Manifest.permission.ACTIVITY_RECOGNITION),
-                Constants.MY_PERMISSIONS_REQUEST_ACTIVITY_RECOGNITION)
-        }else{
-//            pendingIntent.send()
-            // registering client
+                        if (diff_lat<= 1e-6 && diff_long <= 1e-6){
+                            // very less difference between position track of user
+                            // indicates stationary
+                            Log.e(ContentValues.TAG, "User is stationary")
+                            status = 0
+                        }else if (location.speed >= 2 && location.speed <4){
+                            Log.e(ContentValues.TAG, "User is walking")
+                            status = 1
+                        }else if (location.speed >=4 && location.speed <9){
+                            Log.e(ContentValues.TAG, "User is running")
+                            status = 2
+                        }else {
+                            Log.e(ContentValues.TAG, "User is driving")
+                            status = 3
+                        }
+                        //Log.e(TAG, "Driving..${location.speed}")
+                        //Log.e(TAG, "Longitude Latitude: ${location.longitude} ${location.latitude}")
+                        Log.e(ContentValues.TAG, "${locationMem} diff: ${diff_lat} ${diff_long} speed: ${location.speed}")
+                    }else{
+                        locationMem.add(position,Pair(location.latitude, location.longitude))
+                    }
+                    // add rolling buffer updates
+                    if (observationWin.size<maxObservations){
+                        observationWin.add(status)
+                    }else{
+                        observationWin[positionMem] = status
+                        // update the status
+                        val maxOccurrencesEntry = observationWin.groupingBy { it }
+                            .eachCount()
+                            .maxByOrNull { it.value }
 
-//            ActivityRecognition.getClient(this).requestActivityUpdates(
-//                3000,
-//                pendingIntent
-//            ).addOnSuccessListener {
-//                Log.d(ContentValues.TAG, "successful activity recognition listener registration")
-//            }.addOnFailureListener{ e: Exception ->
-//                Log.e(ContentValues.TAG,"Transitions could not be unregistered: $e")
-//            }
-            ActivityRecognition.getClient(this)
-                .requestActivityTransitionUpdates(transitionRequest, pendingIntent)
-                .addOnSuccessListener {
-                    // Handle success
-                    Log.d(ContentValues.TAG, "successful activity recognition listener registration")
+                        recognizedActivity = maxOccurrencesEntry?.key ?: 0
+                        Log.d(ContentValues.TAG, "Observation Win: ${observationWin}")
+                        Log.d(ContentValues.TAG, "Stable Entry: ${recognizedActivity}")
+                    }
                 }
-                .addOnFailureListener { e: Exception ->
-                    // Handle error
-                    Log.e(ContentValues.TAG,"Transitions could not be unregistered: $e")
+            }
+        }
+
+        // granting permissions if not already granted
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ),
+                Constants.REQUEST_CODE
+            )
+        }
+        else{
+            fusedLocationClient.requestLocationUpdates(
+                locationRequest,
+                locationCallback,
+                null
+            )
+        }
+
+        setContent {
+            ActivityRecognitionTheme {
+                // A surface container using the 'background' color from the theme
+                var displayActivity by remember { mutableStateOf(this.recognizedActivity) }
+                displayActivity = recognizedActivity
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Text(text = "Recognized Activity: ${recognizedActivity}")
                 }
+            }
         }
     }
 
-    companion object {
-        private const val TAG = "MainActivity"
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        // unregister on stop
-        unregisterReceiver(reciever)
-    }
 }
 
 
